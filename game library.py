e02ca5fd04a7a57e0ea4ee1b73ff18293addf286
LoganTriplett <55282710+LoganTriplett@users.noreply.github.com>
2020-01-28 09:56:20 -0600

    Add files via upload

--- game library.py
+++ game library.py
@@ -9,118 +9,40 @@ import pickle
 
 games = {1:['FPS','Halo 3','Bungie','Mircosoft','xbox 360','2007','8','either','$6','yes','1/15/2008','This game is great`  ']}
 
-def print_all(sort_it):
-    #print("running print_all()")
-    key_list = GameKeys.keys()
-    if sort_it:
-        sorted_list =[]
-        for key in key_list:
-            sorted_list.append(key)
-        sorted_list.sort()
-        key_list = sorted_list
-
-for key in key_list:
-        print()
-        print("Genre: ", key)
-        print("Title: ", GameKeys[key][1]+", "+GameKeys[key][0])
-        print("Developer: ", GameKeys[key][2])
-        print("publisher: ", GameKeys[key][3])
-        print("System: ", GameKeys[keys][4])
-        print("Release Date: ", GameKeys[keys][5]) 
-        print("Rating: ", GameKeys[keys][6])
-        print("Single/multi/either: ", GameKeys[keys][7])
-
-def lookup():
-    GameKeys = input("What is the GameName ")
-    for Gamekeys in GameKeys.keys():
-        if name == GameKeys[key][1]:
-            found_one = True
-        print()
-        print("Genre: ", key)
-        print("Title: ", GameKeys[key][1]+", "+GameKeys[key][0])
-        print("Developer: ", GameKeys[key][2])
-        print("publisher: ", GameKeys[key][3])
-        print("System: ", GameKeys[keys][4])
-        print("Release Date: ", GameKeys[keys][5]) 
-        print("Rating: ", GameKeys[keys][6])
-        print("Single/multi/either: ", GameKeys[keys][7])
-
-    
-    #if GameName in GameKeys:
-       # print()
-      #  print("Title: ", GameName)
-       # print("Developer: ", GameKeys[GameName][1]+", "+GameKeys[GameName][0])
-        #print("Birth Year: ", GameKeys[GameName][2])
-        #print("publisher: ", GameKeys[GameName][3])
-        #print("System: ", GameKeys[keys][4])
-        #print("Release Date: ", GameKeys[keys][5]) 
-        #print("Rating: ", GameKeys[keys][6])
-        #print("Single/multi/either: ", GameKeys[keys][7])
-
-    else:
-        print("*** GAME NOT FOUND! ***\n")
 
 
-    if not found_one:
-        print("*** NO MATCHES FOUND!***\n")
-
-def add_new():
-    #print("running add_new()")
-    GameKeys = input("What is the SSN of the person to add? ")
-    if GameKeys in people:
-        print("*** THAT GAME ALREADY EXISTS IN THE LIBRARY! ***\n")
-    else:
-        first = input("   What is their first name? ")
-        last = input("   What is their last name? ")
-        year = input("   What is their birth year? ")
-        people[ssn] = [first, last, year]     
-
-def edit_record():
-   GameKeys = input("What is the SSN of the person to edit? ")
-    if ssn not in people:
-        print("*** THAT SSN DOES NOT EXIST! ***\n")
-    else:
-        first = input("  What is their first name? ")
-        last = input( "   What is their last name?  ")
-        year = input( "   What is their birth year?")
-        people[ssn] = [first, last, year]  
-        
-def delete_record():
-    ssn = input("What is the SSN of the person to delete? ")
-    if ssn not in people:
-        print("*** THAT SSN DOES NOT EXIST! ***\n")
-    else:
-        entry = people.pop(ssn)
-        print(ssn, ":", entry[0], entry[1]+", removed.")
-
-def quit():
-    #print("running quit()")
-    pickle_file = open("datafile.pickle", "wb")
-    pickle.dump(people, pickle_file)
-    pickle_file.close()
-    
-def update_people(GameKeys):
-    print()
-        print("Genre: ", key)
-        print("Title: ", GameKeys[key][1]+", "+GameKeys[key][0])
-        print("Developer: ", GameKeys[key][2])
-        print("publisher: ", GameKeys[key][3])
-        print("System: ", GameKeys[keys][4])
-        print("Release Date: ", GameKeys[keys][5]) 
-        print("Rating: ", GameKeys[keys][6])
-        print("Single/multi/either: ", GameKeys[keys][7])
-
-
-GameKeys = {}
-pickle_file = open("datafile.pickle", "rb")
-GameKeys = pickle.load(pickle_file)
+pickle_file = open("./saves/gamelib.pickle", "rb")
+games = pickle.load(pickle_file)
 pickle_file.close()
+
 
 
 keep_going = True
 
+
+def print_all_games():
+
+    key_list = games.keys()
+
+    for key in key_list:
+        print()
+        print("Genre: ", games[key][0])
+        print("Title: ", games[key][1])
+        print("Developer: ", games[key][2])
+        print("Publisher: ", games[key][3])
+        print("Platform: ", games[key][4])
+        print("Year Published: ", games[key][5])
+        print("Rating: ", games[key][6])
+        print("Single, Multiplayer, or Both: ", games[key][7])
+        print("Price: ", games[key][8])
+        print("Completed: ", games[key][9])
+        print("Date of Purchase: ", games[key][10])
+        print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
+
+
 while keep_going:
     print("""
+    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
                                  Welcome to Game Library
     
 
@@ -136,12 +58,34 @@ while keep_going:
     
     """)
 
-     if choice == "1":
-        add_new()
 
+    choice = input("What would you like to do?\n ")
+    print(" ")
+
+    if choice == "1":
+        print("Work in progress")
     elif choice == "2":
-        print_all(False)
+       print_all_games()
+    elif choice == "3":
+        print("Work in progress")
+    elif choice == "4":
+       print("Work in progress")
 
-    elif choice == "3"
+    elif choice == "5":
+        pickle_file = open("./saves/gamelib.pickle", "wb")
+        pickle.dump(games, pickle_file)
+        pickle_file.close()
 
-    elif choice == "4"
+        pickle_file = open("./saves/gamelib.pickle", "rb")
+        games = pickle.load(pickle_file)
+
+        print("SAVING...\n")
+        print(" ")
+        print("SAVING COMPLETE\n")
+
+    elif choice == "Q" or choice == "q":
+        quit()
+        keep_going = False
+
+    else:
+        print("*** NOT A VALID CHOICE ***\n")
