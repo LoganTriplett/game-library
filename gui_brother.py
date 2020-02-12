535f955842beab6091e9ff5f2ac82ae93e010846
LoganTriplett <55282710+LoganTriplett@users.noreply.github.com>
2020-02-12 10:17:25 -0600

    Add files via upload

--- gui_brother.py
+++ gui_brother.py
@@ -1,27 +1,11 @@
 import pickle
 import tkinter as tk
 from tkinter import scrolledtext
+import big_brother
 
 class MainMenu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
-        lbl_title = tk.Label(text = "Game Library",
-                             font = ("Arial", "20"))
-        lbl_title.grid(row = 0, column = 0, sticky = "news")
-        btn_add = tk.Button(text = "Add", command = addscreen)
-        btn_add.grid(row = 1, column = 0)
-        btn_edit = tk.Button(text = "Edit")
-        btn_edit.grid(row = 2, column = 0)
-        btn_search = tk.Button(text = "Search")
-        btn_search.grid(row = 3, column = 0)
-        btn_remove = tk.Button(text = "Remove")
-        btn_remove.grid(row = 4, column = 0)
-        btn_save = tk.Button(text = "Save")
-        btn_save.grid(row = 5, column = 0)
-    
-
-class addscreen():
-    def __init__(self):
         lbl_title = tk.Label(text = "Game Library",
                              font = ("Arial", "20"))
         lbl_title.grid(row = 0, column = 0, sticky = "news")
@@ -35,6 +19,18 @@ class addscreen():
         btn_remove.grid(row = 4, column = 0)
         btn_save = tk.Button(text = "Save")
         btn_save.grid(row = 5, column = 0)
+        self.btn_one = tk.Button(self, text = "Hi")
+        self.btn_one.grid(row = 0, column = 0)
+        self.btn_two = tk.Button(self, text = "Howdy!!")
+        self.btn_two.grid(row = 0, column = 1)
+
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)
+
+    
+
+
+
 
 
 if __name__ == "__main__":
